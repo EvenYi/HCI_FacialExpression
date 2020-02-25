1f271f0a43759f5107c4047fa5a278f292692618
Hanlin <41379917+GaoHanlin@users.noreply.github.com>
2020-02-25 17:30:46 -0500

    surprise v1

--- p4-skeleton.py
+++ p4-skeleton.py
@@ -2,7 +2,6 @@
 
 from subprocess import Popen, DEVNULL
 import shlex, os, errno, time, glob
-import numpy as np
 
 # Constants for later use
 of2_verbose = False
@@ -26,7 +25,7 @@ except OSError as e:
         raise  # re-raise exception if a different error occurred
 
 # These lines write the command to run openface with the correct options
-command = shlex.split(" -device 1 -out_dir . -pose -2Dfp -of " + temp_output)
+command = shlex.split(" -device 0 -out_dir . -pose -2Dfp -of " + temp_output)
 command.insert(0, exe)
 
 # This line starts openface
@@ -38,6 +37,8 @@ while not os.path.exists(temp_output_file):
 
 # Openface saves info to a file, and we open that file here
 data = open(temp_output_file, 'r')
+# surprise = open('surprise.txt', 'w')
+
 
 # This loop repeats while openface is still running
 # Inside the loop, we read from the file that openface outputs to and check to see if there's anything new
@@ -45,9 +46,14 @@ data = open(temp_output_file, 'r')
 list_pitch = []
 list_yaw = []
 list_roll = []
-list_m_x_48_54 = []
-list_m_y_51_57 = []
-list_be_y_24_43 = []
+brow_dist1_list = []
+brow_dist2_list = []
+eye_dist1_list = []
+eye_dist2_list = []
+mouth_long_list = []
+mouth_width_list = []
+pose_Tz_list = []
+i = 0
 while (of2.poll() == None):
     line = data.readline().strip()
 
@@ -58,24 +64,47 @@ while (of2.poll() == None):
             # Yes No Indian Nod
             timestamp, confidence, success = of_values[2:5]
             pitch, yaw, roll = of_values[8:11]
-            # print(len(of_values))
+
+            # no_data.write(str(yaw))
+            # indian_nod.write(str(roll))
+            #print(len(of_values))
 
             # Smile
 
-            m_y_51 = of_values[130]
-            m_y_57 = of_values[136]
-            m_x_48 = of_values[59]
-            m_x_54 = of_values[65]
-            e_y_37 = of_values[116]
-            e_y_41 = of_values[120]
+            '''
+            m_x_48 = of_values[269]
+            m_x_54 = of_values[275]
+
+            e_y_37 = of_values[258]
+            e_y_41 = of_values[262]
+            e_y_38 = of_values[259]
+            e_y_40 = of_values[261]
+
+            e_y_43 = of_values[264]
+            e_y_47 = of_values[268]
+            e_y_44 = of_values[265]
+            e_y_46 = of_values[267]
+
+            b_y_23 = of_values[244]
+            b_y_20 = of_values[241]
+            '''
+
+            # surprise
+            b_y_19 = of_values[98]
             e_y_38 = of_values[117]
             e_y_40 = of_values[119]
+
+            b_y_24 = of_values[103]
             e_y_43 = of_values[122]
             e_y_47 = of_values[126]
-            e_y_44 = of_values[123]
-            e_y_46 = of_values[125]
-            b_y_24 = of_values[103]
-            b_y_19 = of_values[98]
+
+            m_y_51 = of_values[130]
+            m_y_57 = of_values[136]
+
+            m_x_48 = of_values[59]
+            m_x_54 = of_values[65]
+
+            pose_Tz = of_values[7]
 
             landmarks = []
             for i in range(11, 11 + landmark_count):
@@ -86,40 +115,78 @@ while (of2.poll() == None):
 
         # ********************************************
         # Most, maybe all, of your code will go here
-        if len(list_pitch) < 12:
-            list_pitch.append(pitch)
-            list_yaw.append(yaw)
-            list_roll.append(roll)
-            list_m_x_48_54.append(abs(m_x_48 - m_x_54))
-            list_m_y_51_57.append(abs(m_y_51 - m_y_57))
-            list_be_y_24_43.append(abs(b_y_24 - e_y_43))
+
+        # if len(list_pitch) < 12:
+        #     list_pitch.append(pitch)
+        #     list_yaw.append(yaw)
+        #     list_roll.append(roll)
+        # else:
+        #     # print(abs(b_y_19 - b_y_38))
+        #     # print(abs(b_y_24 - b_y_43))
+        #
+        #     # print(m_y_51, m_y_57);
+        #
+        #     # print("pitch", max(list_pitch), min(list_pitch))
+        #     #print("pitch", abs(max(list_pitch) - min(list_pitch)))
+        #     #print("yaw", abs(max(list_yaw) - min(list_yaw)))
+        #     #print("roll", abs(max(list_roll) - min(list_roll)))
+        #
+        #     if abs(max(list_pitch) - min(list_pitch)) >= 0.365 and abs(max(list_yaw) - min(list_yaw)) < 0.15 and abs(
+        #             max(list_roll) - min(list_roll)) < 0.15:
+        #         print("Yes")
+        #     if abs(max(list_yaw) - min(list_yaw)) >= 0.3 and abs(max(list_pitch) - min(list_pitch)) < 0.15 and abs(
+        #             max(list_roll) - min(list_roll)) < 0.15:
+        #         print("No")
+        #     if abs(max(list_roll) - min(list_roll)) >= 0.3 and abs(max(list_pitch) - min(list_pitch)) < 0.15 and abs(
+        #         max(list_yaw) - min(list_yaw)) < 0.15:
+        #         print("Indian Nod")
+
+            # list_pitch = []
+            # list_yaw = []
+            # list_roll = []
+        brow_dist1 = abs(b_y_19 - e_y_38)
+        brow_dist2 = abs(b_y_24 - e_y_43)
+        eye_dist1 = abs(e_y_38 - e_y_40)
+        eye_dist2 = abs(e_y_43 - e_y_47)
+        mouth_long = abs(m_y_51 - m_y_57)
+        mouth_width = abs(m_x_48 - m_x_54)
+
+        # surprise.write(str(brow_dist1) + '  ')
+        # surprise.write(str(brow_dist2) + '  ')
+        # surprise.write(str(eye_dist1) + '   ')
+        # surprise.write(str(eye_dist2) + '   ')
+        # surprise.write(str(mouth_long) + '  ')
+        # surprise.write(str(mouth_width) + '\n')
+
+        if len(brow_dist1_list) < 12:
+            brow_dist1_list.append(brow_dist1)
+            brow_dist2_list.append(brow_dist2)
+            eye_dist1_list.append(eye_dist1)
+            eye_dist2_list.append(eye_dist2)
+            mouth_long_list.append(mouth_long)
+            mouth_width_list.append(mouth_width)
+            pose_Tz_list.append(pose_Tz)
         else:
 
-            if np.var(list_pitch) >= 0.005 and abs(max(list_yaw) - min(list_yaw)) < 0.15 and abs(
-                    max(list_roll) - min(list_roll)):
-                print("Yes")
-            elif abs(max(list_yaw) - min(list_yaw)) >= 0.25 and abs(max(list_pitch) - min(list_pitch)) < 0.1 and abs(
-                    max(list_roll) - min(list_roll)) < 0.1:
-                print("No")
-            elif abs(max(list_roll) - min(list_roll)) >= 0.17 and abs(max(list_pitch) - min(list_pitch)) < 0.15 and abs(
-                    max(list_yaw) - min(list_yaw)) < 0.15:
-                print("Indian Nod")
+            brow_abs1 = max(brow_dist1_list) - min(brow_dist1_list)
+            brow_abs2 = max(brow_dist2_list) - min(brow_dist2_list)
 
-            elif abs(max(list_yaw) - min(list_yaw)) < 0.1 and abs(max(list_pitch) - min(list_pitch)) < 0.1 and abs(
-                    max(list_roll) - min(list_roll)) < 0.1 and abs(
-                max(list_m_y_51_57) - min(list_m_y_51_57)) > 1 and abs(
-                max(list_m_y_51_57) - min(list_m_y_51_57)) < 10 and abs(
-                max(list_m_x_48_54) - min(list_m_x_48_54)) > 6 and abs(max(list_be_y_24_43) - min(list_be_y_24_43)) < 2:
-                print("Smile")
+            eye_abs1 = max(eye_dist1_list) - min(eye_dist1_list)
+            eye_abs2 = max(eye_dist2_list) - min(eye_dist2_list)
 
-            list_pitch = []
-            list_yaw = []
-            list_roll = []
-            list_m_x_48_54 = []
-            list_m_y_51_57 = []
-            list_be_y_24_43 = []
+            mouth_dist_abs = max(mouth_long_list) - min(mouth_long_list)
+            pose_Tz_abs = max(pose_Tz_list) - min(pose_Tz_list)
 
+            if brow_abs1 >= 3 and brow_abs2 >= 3 and eye_abs1 >= 0.3 and eye_abs2 >= 0.3 and 15 >= mouth_dist_abs >= 2 and pose_Tz_abs <= 30:
+                print('SURPRISE!!!', i)
+                i += 1
 
+            brow_dist1_list = []
+            brow_dist2_list = []
+            eye_dist1_list = []
+            eye_dist2_list = []
+            mouth_long_list = []
+            pose_Tz_list = []
 
     # ********************************************
 
